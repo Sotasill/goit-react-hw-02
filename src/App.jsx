b8d5a0c89f5a2b9fcf3445aafa85198ec27b77c0
SotaSill <sotasill@gmail.com>
2024-09-28 00:05:47 +0300

    2

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import "modern-normalize/modern-normalize.css";
+import "./App.css";
+import Description from "./Description/Description";
+import Options from "./Options/Options";
+import Feedback from "./Feedback/Feedback";
+
+
+const startPosition = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const storagedFeedback = localStorage.getItem("feedback");
+    if (storagedFeedback !== null) {
+      return JSON.parse(storagedFeedback);
+    }
+    return startPosition;
+  });
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  useEffect(() => {
+    document.title = totalFeedback
+      ? `Positive feedback ${positiveFeedback}%`
+      : "No feedback yet";
+  }, [positiveFeedback, totalFeedback]);
+
+  function updateFeedback(feedbackType) {
+    setFeedback({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1,
+    });
+  }
+
+  function resetFeedback() {
+    setFeedback(startPosition);
+  }
+
+  return (
+    <div className="container">
+      <Description />
+      <Options
+        options={Object.keys(feedback)}
+        setter={updateFeedback}
+        total={totalFeedback}
+        reset={resetFeedback}
+      />
+      {totalFeedback ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positive={positiveFeedback}
+        />
+      ) : (
+        <p>No feedback given yet</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
